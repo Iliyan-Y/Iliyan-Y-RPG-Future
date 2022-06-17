53e316dc5ce929a753ce6244bb7e47fa31bed093
Iliyan-Y <maximizebg@gmail.com>
2022-06-17 17:06:15 +0100

    Socket io and Unity connected

--- server/index.js
+++ server/index.js
@@ -5,12 +5,44 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.use((socket, next) => {
+//   if (socket.handshake.query.token === 'UNITY') {
+//     next();
+//   } else {
+//     next(new Error('Authentication error'));
+//   }
+// });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('connection');
+  console.log(socket.handshake.query.token);
+
+  setTimeout(() => {
+    socket.emit('connection', {
+      date: new Date().getTime(),
+      data: 'Hello Unity',
+    });
+  }, 1000);
+
+  socket.on('hello', (data) => {
+    console.log('hello', data);
+    socket.emit('hello', { date: new Date().getTime(), data: data });
+  });
+
+  socket.on('spin', (data) => {
+    console.log('spin');
+    socket.emit('spin', { date: new Date().getTime(), data: data });
+  });
+
+  socket.on('class', (data) => {
+    console.log('class', data);
+    socket.emit('class', { date: new Date().getTime(), data: data });
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });

c2c96290e2308221450b6f7c491e0043a6d4bdef
Iliyan-Y <maximizebg@gmail.com>
2022-06-24 17:31:09 +0100

    Text area fixed and progress bar created

--- game/Assets/Scripts/Home/LogIn.cs
+++ game/Assets/Scripts/Home/LogIn.cs
@@ -2,20 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LogIn : MonoBehaviour
 {
+  public Image loadingProgressBar;
+  public GameObject home;
+  public GameObject loadingInterface;
 
-  public void GameLogIn()
+  List<AsyncOperation> sceneToLoad = new List<AsyncOperation>();
+  public void StartGame()
   {
-    AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Space");
-
-    // Wait until the asynchronous scene fully loads
-    while (!asyncLoad.isDone)
-    {
-      Debug.Log("Loading...");
-      //  yield return null;
-    }
+    HideHome();
+    ShowLoading();
+    sceneToLoad.Add(SceneManager.LoadSceneAsync("Space"));
+    StartCoroutine(LoadingScreen());
   }
+
+  void HideHome()
+  {
+    home.SetActive(false);
+  }
+
+  void ShowLoading()
+  {
+    loadingInterface.SetActive(true);
+  }
+
+  IEnumerator LoadingScreen()
+  {
+    float totalProgress = 0;
+
+    for (int i = 0; i < sceneToLoad.Count; i++)
+    {
+      while (!sceneToLoad[i].isDone)
+      {
+        totalProgress += sceneToLoad[i].progress;
+        loadingProgressBar.fillAmount = totalProgress / sceneToLoad.Count;
+        yield return null;
+      }
+    }
+
+  }
+
+  // public void GameLogIn()
+  // {
+  //   AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Space");
+
+  //   // Wait until the asynchronous scene fully loads
+  //   while (!asyncLoad.isDone)
+  //   {
+  //     Debug.Log("Loading...");
+  //     //  yield return null;
+  //   }
+  // }
 
 }

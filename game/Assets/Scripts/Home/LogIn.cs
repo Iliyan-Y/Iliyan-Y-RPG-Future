a8abaf2efa535e6d70f47a825c9da898e1699f2e
iliyan <maximizebg@gmail.com>
2022-06-24 11:28:01 +0100

    Login script created

---
+++ game/Assets/Scripts/Home/LogIn.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogIn : MonoBehaviour
+{
+
+  public void GameLogIn()
+  {
+    SceneManager.LoadScene("Space");
+  }
+
+}
